adcc2d879f18f90c4ae5142f413557c7716ada5d
csae8092 <p.andorfer@gmail.com>
2025-11-10 08:48:09 +0100

    minor

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 echo "converting legacy data into TEIs"
 ant
+echo "########"
 echo "create listbibl.xml"
-uv run pyscripts/titles.py 
+uv run pyscripts/titles.py
+echo "########"
+echo "adding next/prev issue/halbband/band"
+uv run pyscripts/navigation.py
+echo "########"
